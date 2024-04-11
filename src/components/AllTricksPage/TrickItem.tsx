7967b320d63f437091945184eeffe7d476bf87fc
Scott Ertmer <scottertmer@gmail.com>
2024-04-11 15:37:03 -0600

    Add styles for larger screens

--- src/components/AllTricksPage/TrickItem.tsx
+++ src/components/AllTricksPage/TrickItem.tsx
@@ -12,7 +12,7 @@ const TrickItem = ({trickInfo}) => {
 
   
   return (
-    <div className="border border-black p-4 min-w-max sm:min-w-0 sm:w-64">
+    <div className="border border-black p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
       <h3 className="text-base sm:text-lg font-bold">{name}</h3>
       <p className="text-sm sm:text-base">{trickType}</p>
       <p className="text-sm sm:text-base">{difficulty}</p>
