b280c31e56c439e84aaa67bc6ef86730d7fb7f85
Scott Ertmer <scottertmer@gmail.com>
2024-04-22 14:27:20 -0600

    Add conditional rendering

--- src/app/tricks/[id]/page.tsx
+++ src/app/tricks/[id]/page.tsx
@@ -8,6 +8,10 @@ const Page = () => {
   const id = params.id
   const matchingTrick = tricks.find(trick => id === trick.trickId.toString())
 
+  if (!matchingTrick) {
+    return <p>No Trick Found</p>
+  } 
+
   return (
     <div className="border border-black p-4 w-full sm:w-1/2 md:w-1/3 lg:w-1/4 xl:w-1/5">
       <h3 className="text-base sm:text-lg font-bold">{matchingTrick.name}</h3>
