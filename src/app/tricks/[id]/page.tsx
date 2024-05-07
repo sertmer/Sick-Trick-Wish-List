c3c3303c74cddfd7ebf9ba9627f0e4b50db4aa31
Scott Ertmer <scottertmer@gmail.com>
2024-05-07 10:53:56 -0600

    add type to params

--- src/app/tricks/[id]/page.tsx
+++ src/app/tricks/[id]/page.tsx
@@ -3,7 +3,7 @@ import '@/components/Tricks/TrickItem';
 import TrickItem from '@/components/Tricks/TrickItem';
 import {fetchTrickById} from '../../../../data/data';
 
-async function Page({params}) {
+async function Page({params}: {params: {id: string}}) {
   const id = params.id
   const trick = await fetchTrickById(id)
   // add render for undefined trick
