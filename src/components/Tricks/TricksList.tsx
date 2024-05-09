e06e80bf34ef8719e5f94896676795854646e41c
Scott Ertmer <scottertmer@gmail.com>
2024-05-09 11:04:07 -0600

    Filter tricks by type

--- src/components/Tricks/TricksList.tsx
+++ src/components/Tricks/TricksList.tsx
@@ -1,29 +1,45 @@
 import TrickItem from '@/components/Tricks/TrickItem';
 import { Trick } from '../../../types/types';
 import Link from 'next/link';
-import {fetchTricks} from '../../../data/data';
-
-// interface TricksListProps {
-//   tricks: Trick[]
-// }
+import { fetchTricks } from '../../../data/data';
 
 async function TricksList() {
 
-  const tricks = await fetchTricks();
+  const tricks = await fetchTricks(); // get all tricks
+  
+  const filterTricksByType = (trickType: string): Trick[] => {
+    return tricks.filter(trick => trick.trick_type === trickType);
+  };
 
-  const tricksList = tricks.map(trick => {
-    return (
-      <Link href={`/tricks/${trick.trick_id}`} key={trick.trick_id}>
-        <TrickItem trickInfo={trick} />
-      </Link>
-    )
-  })
+  const basic = filterTricksByType('Basic');
+  const flip = filterTricksByType('Flip')
+  const grind = filterTricksByType('Grind')
+
+  const createComponents = (tricks: Trick[]) => {
+    return tricks.map(trick => {
+      return (
+        <Link href={`/tricks/${trick.trick_id}`} key={trick.trick_id}>
+          <TrickItem trickInfo={trick} />
+        </Link>
+      )
+    })
+  }
+
+  const basicTricks = createComponents(basic)
+  const flipTricks = createComponents(flip)
+  const grindTricks = createComponents(grind)
+
 
   return (
     <div>
-      <p>TricksList</p>
       <div className="flex overflow-x-auto py-2 sm:py-4 space-x-2 sm:space-x-4">
-        {tricksList}
+        {basicTricks}
+      </div>
+      <div className="flex overflow-x-auto py-2 sm:py-4 space-x-2 sm:space-x-4">
+        {flipTricks}
+      </div>
+      <div className="flex overflow-x-auto py-2 sm:py-4 space-x-2 sm:space-x-4">
+        {grindTricks}
       </div>
     </div>
   )
