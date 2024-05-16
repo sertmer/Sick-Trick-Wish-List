7950ef7ef0b8590a7f01a0edf1dd81e61599061f
Scott Ertmer <scottertmer@gmail.com>
2024-05-16 10:39:02 -0600

    Move fetch to page

--- src/app/tricks/page.tsx
+++ src/app/tricks/page.tsx
@@ -1,9 +1,12 @@
 import TricksList from '@/components/Tricks/TricksList';
+import { fetchTricks } from '../../../data/data';
+import { Trick } from '../../../types/types';
 
-const Page = () => {
+async function Page() {
+  const tricks = await fetchTricks();
   return (
     <div>
-    <TricksList/>
+    <TricksList tricks={tricks}/>
     </div>
   )
 }
